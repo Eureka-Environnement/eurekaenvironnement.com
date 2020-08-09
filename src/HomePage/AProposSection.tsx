562f8db3742fc311cccf669da83445170077ec98
Bryan Lim kee chang <bryanlim@hotmail.ca>
2020-08-08 21:41:17 -0400

    Format

--- src/HomePage/AProposSection.tsx
+++ src/HomePage/AProposSection.tsx
@@ -1,17 +1,11 @@
 import React from "react";
-import {
-  Col,
-  Container,
-  Row
-} from "reactstrap";
+import { Col, Container, Row } from "reactstrap";
 
 interface AProposSection {
   aProposRef: React.MutableRefObject<HTMLDivElement | null>;
 }
 
-const AProposSection = ({
-  aProposRef
-}: AProposSection) => {
+const AProposSection = ({ aProposRef }: AProposSection) => {
   return (
     <div className="section text-center" ref={aProposRef}>
       <Container>
@@ -19,7 +13,15 @@ const AProposSection = ({
           <Col className="ml-auto mr-auto" md="12">
             <h2 className="title">À Propos</h2>
             <h5 className="description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rhoncus, purus sit amet iaculis finibus, diam urna consectetur leo, at eleifend sapien nulla eget neque. Aenean fringilla dapibus vulputate. Donec imperdiet erat a magna iaculis efficitur. Fusce egestas risus lobortis erat pulvinar, ac posuere nibh blandit. Praesent faucibus mattis odio et aliquet. Aenean at lacus rutrum, elementum velit pretium, molestie justo. Etiam pharetra tincidunt consequat. Nunc diam arcu, iaculis sed semper posuere, posuere euismod augue.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              rhoncus, purus sit amet iaculis finibus, diam urna consectetur
+              leo, at eleifend sapien nulla eget neque. Aenean fringilla dapibus
+              vulputate. Donec imperdiet erat a magna iaculis efficitur. Fusce
+              egestas risus lobortis erat pulvinar, ac posuere nibh blandit.
+              Praesent faucibus mattis odio et aliquet. Aenean at lacus rutrum,
+              elementum velit pretium, molestie justo. Etiam pharetra tincidunt
+              consequat. Nunc diam arcu, iaculis sed semper posuere, posuere
+              euismod augue.
             </h5>
           </Col>
         </Row>
