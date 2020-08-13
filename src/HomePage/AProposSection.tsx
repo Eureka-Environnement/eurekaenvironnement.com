369afa403f765d26c442733535c11195057bae17
Bryan Lim kee chang <bryanlim@hotmail.ca>
2020-08-13 18:35:03 -0400

    UI upgrade

--- src/HomePage/AProposSection.tsx
+++ src/HomePage/AProposSection.tsx
@@ -1,53 +1,49 @@
 import React from "react";
-import {
-  Col,
-  Container,
-  Row,
-  Card,
-  CardImg,
-  CardBody,
-  CardText,
-} from "reactstrap";
+import { Col, Container, Row, UncontrolledCarousel } from "reactstrap";
 
 interface AProposSection {
   aProposRef: React.MutableRefObject<HTMLDivElement | null>;
 }
 
+const carouselItems = [
+  {
+    src:
+      "http://localhost:3000/eurekaenvironnement.com/img/riviere-eaux-traitement.jpg",
+    altText: "Slide 1",
+    caption: "",
+  },
+  {
+    src:
+      "http://localhost:3000/eurekaenvironnement.com/img/traitement-eaux-residentiel.jpg",
+    altText: "Slide 1",
+    caption: "",
+  },
+];
+
 const AProposSection = ({ aProposRef }: AProposSection) => {
   return (
-    <div className="section text-center section-full-viewport" ref={aProposRef}>
+    <div className="section text-center section-full-viewport section-a-propos" ref={aProposRef}>
       <Container>
         <Row>
           <Col className="ml-auto mr-auto" md="12">
-            <h1 className="title">À Propos</h1>
+            <h1 className="title title-spacing">À Propos</h1>
           </Col>
         </Row>
         <Row>
-          <Col md={{ size: '5'}}>
-            <Card className="adjust-to-text">
-              <CardImg
-                top
-                src="http://localhost:3000/eurekaenvironnement.com/img/riviere-eaux-traitement.jpg"
-                alt="..."
-              />
-              <CardBody>
-                <CardText>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </CardText>
-              </CardBody>
-            </Card>
+          <Col md="6">
+            <UncontrolledCarousel items={carouselItems} />
           </Col>
-          <Col md={{ size: '5', offset: 2 }}>
+          <Col md="6">
             <h4 className="description">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-              rhoncus, purus sit amet iaculis finibus, diam urna consectetur
-              leo, at eleifend sapien nulla eget neque. Aenean fringilla dapibus
-              vulputate. Donec imperdiet erat a magna iaculis efficitur. Fusce
-              egestas risus lobortis erat pulvinar, ac posuere nibh blandit.
-              Praesent faucibus mattis odio et aliquet. Aenean at lacus rutrum,
-              elementum velit pretium, molestie justo. Etiam pharetra tincidunt
-              consequat. Nunc diam arcu, iaculis sed semper posuere, posuere
-              euismod augue.
+              Eurêka Environnement est une firme de génie-conseil spécialisée
+              dans la gestion de l’eau et la conception de systèmes de
+              traitement. Que vous souhaitiez prélever de l’eau d’un puits, la
+              rendre potable, traiter vos eaux usées, gérer et réutiliser vos
+              eaux pluviales, notre expertise vous permettra de réaliser tous
+              vos projets. Notre mission : vous offrir un service personnalisé
+              adapté à vos besoins ainsi qu’aux enjeux environnementaux actuels
+              et futurs. Eurêka Environnement, des solutions innovantes à vos
+              problèmes les plus complexes.
             </h4>
           </Col>
         </Row>
