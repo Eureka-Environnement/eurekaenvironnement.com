dab996365704abffbdf524047b26d49d1bbceb27
Sebastien Deschambault <39055954+sebd11@users.noreply.github.com>
2020-11-09 21:42:02 -0500

    Changes after feedback (#17)
    * adjustements after feedback
    
    * contact section

--- src/HomePage/NousJoindreSection.tsx
+++ src/HomePage/NousJoindreSection.tsx
@@ -1,199 +1,66 @@
-import React, { useState } from "react";
-import { toast } from "react-toastify";
+import React from "react";
 import {
-  Button,
   Col,
   Container,
-  Input,
-  InputGroup,
-  Label,
   Row,
 } from "reactstrap";
-import { FaFacebookSquare, FaLinkedin, FaPhone } from "react-icons/fa";
-import { MdEmail } from "react-icons/md";
-
-const ToastConfigs = {
-  draggable: false,
-  closeOnClick: true,
-  position: toast.POSITION.BOTTOM_RIGHT,
-  autoClose: 5000,
-};
 
 interface NousJoindreSection {
   nousJoindreRef: React.MutableRefObject<HTMLDivElement | null>;
 }
 
 const NousJoindreSection = ({ nousJoindreRef }: NousJoindreSection) => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [subject, setSubject] = useState("");
-  const [message, setMessage] = useState("");
-
-  const resetInputs = () => {
-    setName("");
-    setEmail("");
-    setSubject("");
-    setMessage("");
-  };
-
-  const sendEmail = () => {
-    if (name && email && subject && message) {
-      toast.success("Courriel envoyé!", ToastConfigs);
-      resetInputs();
-    } else {
-      toast.error("Veuillez remplir toutes les sections.", ToastConfigs);
-    }
-  };
-
   return (
     <div className="section text-center" ref={nousJoindreRef}>
       <Container>
         <Row>
-          <Col className="ml-auto mr-auto" md="8">
-            <h1 className="title eureka">Pour nous rejoindre</h1>
+          <Col className="ml-auto mr-auto" md="12">
+            <h1 className="title eureka">Contactez-nous !</h1>
+            <hr />
           </Col>
         </Row>
         <Row>
-          <Col md="3" className="center-horizontal">
-            <Row className="icon icon-info center-horizontal">
-              <FaPhone
-                color="#01184f"
-                size="55px"
-                className="center-horizontal phone-icon"
-              />
-            </Row>
-            <Row className="center-horizontal">
-              <p className="description center-horizontal">
-                450-300-4045 | 819-415-5535
-              </p>
-            </Row>
+          <Col md="6">
+            <h3 style={{ textAlign: 'left', color: '#01184f', fontWeight: 'initial' }}>Téléphone</h3>
+            <p className="description center-horizontal">
+              450-300-4045 | 819-415-5535
+            </p>
           </Col>
-          <Col md="3" className="center-horizontal">
-            <a href="mailto:info@eurekaenvironnement.com">
-              <Row className="icon icon-info center-horizontal">
-                <MdEmail
-                  color="#01184f"
-                  size="60px"
-                  className="center-horizontal"
-                />
-              </Row>
-              <Row className="center-horizontal">
-                <p className="description center-horizontal">
-                  info@eurekaenvironnement.com
-                </p>
-              </Row>
-            </a>
-          </Col>
-          <Col md="3" className="center-horizontal">
-            <a
-              className="mr-1 btn btn-link"
-              title="Suivez nous sur Facebook"
-              href="https://www.facebook.com/eurekaenv/"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <Row className="icon icon-info center-horizontal">
-                <FaFacebookSquare
-                  color="#01184f"
-                  size="60px"
-                  className="center-horizontal"
-                />
-              </Row>
-              <Row className="center-horizontal">
-                <p className="description center-horizontal lowercase-social">
-                  eurekaenv
-                </p>
-              </Row>
-            </a>
-          </Col>
-          <Col md="3" className="center-horizontal">
-            <a
-              className="mr-1 btn btn-link"
-              title="Suivez nous sur Linkedin"
-              href="https://www.linkedin.com/company/eurekaenvironnement"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <Row className="icon icon-info center-horizontal">
-                <FaLinkedin
-                  color="#01184f"
-                  size="60px"
-                  className="center-horizontal"
-                />
-              </Row>
-              <Row className="center-horizontal">
-                <p className="description center-horizontal lowercase-social">
-                  eurekaenvironnement
-                </p>
-              </Row>
-            </a>
+          <Col md="6">
+            <h3 style={{ textAlign: 'left', color: '#01184f', fontWeight: 'initial' }}>Courriel</h3>
+            <p className="description center-horizontal">
+            info@eurekaenvironnement.com
+            </p>
           </Col>
         </Row>
         <Row>
-          <Col className="ml-auto mr-auto" md="8">
-            <div className="contact-form">
-              <Row>
-                <Col md="6">
-                  <Label>Nom</Label>
-                  <InputGroup>
-                    <Input
-                      placeholder="Nom"
-                      type="text"
-                      value={name}
-                      onChange={(e) => {
-                        setName(e.target.value);
-                      }}
-                    />
-                  </InputGroup>
-                </Col>
-                <Col md="6">
-                  <Label>Courriel</Label>
-                  <InputGroup>
-                    <Input
-                      placeholder="Courriel"
-                      type="text"
-                      value={email}
-                      onChange={(e) => {
-                        setEmail(e.target.value);
-                      }}
-                    />
-                  </InputGroup>
-                </Col>
-              </Row>
-              <Label>Sujet</Label>
-              <InputGroup>
-                <Input
-                  placeholder="Sujet"
-                  type="text"
-                  value={subject}
-                  onChange={(e) => {
-                    setSubject(e.target.value);
-                  }}
-                />
-              </InputGroup>
-              <Label>Message</Label>
-              <Input
-                placeholder="Message"
-                type="textarea"
-                rows="6"
-                value={message}
-                onChange={(e) => {
-                  setMessage(e.target.value);
-                }}
-              />
-              <Row>
-                <Col className="ml-auto mr-auto center" md="4">
-                  <Button
-                    className="btn-fill"
-                    color="primary"
-                    size="lg"
-                    onClick={sendEmail}
-                  >
-                    Envoyer
-                  </Button>
-                </Col>
-              </Row>
-            </div>
+          <Col md="6">
+            <h3 style={{ textAlign: 'left', color: '#01184f', fontWeight: 'initial' }}>Siège social</h3>
+            <iframe
+              title="adress"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2755.404773397074!2d-72.56661770152608!3d46.32165754171248!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc7cfa333877951%3A0xd04cc16fbeda7788!2sEur%C3%AAka%20Environnement!5e0!3m2!1sfr!2sca!4v1604973324074!5m2!1sfr!2sca"
+              width="500"
+              height="375"
+              frameBorder="0"
+              style={{ border: 0 }}
+              allowFullScreen
+              aria-hidden="false"
+              tabIndex={0}
+            />
+          </Col>
+          <Col md="6">
+          <h3 style={{ textAlign: 'left', color: '#01184f', fontWeight: 'initial' }}>Trois-Rivières</h3>
+            <iframe
+              title="adress"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2755.404773397074!2d-72.56661770152608!3d46.32165754171248!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc7cfa333877951%3A0xd04cc16fbeda7788!2sEur%C3%AAka%20Environnement!5e0!3m2!1sfr!2sca!4v1604973324074!5m2!1sfr!2sca"
+              width="500"
+              height="375"
+              frameBorder="0"
+              style={{ border: 0 }}
+              allowFullScreen
+              aria-hidden="false"
+              tabIndex={0}
+            />
           </Col>
         </Row>
       </Container>
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react';
+import {
+  Container
+} from 'reactstrap';
+import { FaFacebookSquare, FaLinkedin } from "react-icons/fa";
+
+const Footer = () => {
+  useEffect(() => {
+    function scrollOnClick() {
+      setTimeout(() => window.scroll({
+        top: document.body.scrollHeight,
+        behavior: 'smooth'
+      }), 100);
+    }
+    document.getElementById('contactBtn')?.addEventListener('click', scrollOnClick);
+
+    return function cleanup() {
+      document.getElementById('contactBtn')?.removeEventListener('click', scrollOnClick);
+    };
+  }, []);
+  return (
+    <footer className="footer footer-black footer-white">
+      <Container className="footer-max-width">
+        <div className="credits ml-auto">
+          <a
+            className="mr-1 btn btn-link"
+            title="Suivez nous sur Facebook"
+            href="https://www.facebook.com/eurekaenv/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <FaFacebookSquare size="36" />
+          </a>
+          <a
+            className="mr-1 btn btn-link"
+            title="Suivez nous sur Linkedin"
+            href="https://www.linkedin.com/company/eurekaenvironnement"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <FaLinkedin size="36" />
+          </a>
+        </div>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
